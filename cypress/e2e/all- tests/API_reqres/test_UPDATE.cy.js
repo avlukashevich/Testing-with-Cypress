e9e6be30cb52c9898d6b3a5295c4ff7511026d3d
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:31:51 +0300

    Update test_UPDATE.cy.js

--- cypress/e2e/all- tests/API_reqres/test_UPDATE.cy.js
+++ cypress/e2e/all- tests/API_reqres/test_UPDATE.cy.js
@@ -2,9 +2,9 @@ context('Testing PUT and PATCH methods in reqres', () => {
   it('Positive: update a user by id', () => {
     cy.fixture('updateduser').then((updateduser) => {
       cy.request('PUT', '/api/users/2', updateduser).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body.name).to.eq(updateduser.name);
-        expect(response.body.job).to.eq(updateduser.job);
+        expect(response.status).to.eq(200)
+        expect(response.body.name).to.eq(updateduser.name)
+        expect(response.body.job).to.eq(updateduser.job)
       })
     })
   })
@@ -14,9 +14,9 @@ context('Testing PUT and PATCH methods in reqres', () => {
       name: 'morpheus',
       job: 'zion resident',
     }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.name).to.eq('morpheus');
-      expect(response.body.job).to.eq('zion resident');
+      expect(response.status).to.eq(200)
+      expect(response.body.name).to.eq('morpheus')
+      expect(response.body.job).to.eq('zion resident')
     })
   })
 })
