f5026681fcf0cfb3319281bc32e9e99078bb0151
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:26:24 +0300

    Update test_UPDATE.cy.js

--- cypress/e2e/all- tests/API_reqres/test_UPDATE.cy.js
+++ cypress/e2e/all- tests/API_reqres/test_UPDATE.cy.js
@@ -1,25 +1,22 @@
 context('Testing PUT and PATCH methods in reqres', () => {
-
   it('Positive: update a user by id', () => {
-    cy.fixture('updateduser').then(updateduser => {
-
-      cy.request('PUT', '/api/users/2', updateduser).then(Response => {
-        expect(Response.status).to.eq(200)
-        expect(Response.body.name).to.eq(updateduser.name)
-        expect(Response.body.job).to.eq(updateduser.job)
-      })
-    })
-  })
+    cy.fixture('updateduser').then((updateduser) => {
+      cy.request('PUT', '/api/users/2', updateduser).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.name).to.eq(updateduser.name);
+        expect(response.body.job).to.eq(updateduser.job);
+      });
+    });
+  });
 
   it('Positive: update a user by id', () => {
     cy.request('PUT', '/api/users/2', {
-      'name': 'morpheus',
-      'job': 'zion resident'
-    }
-    ).then(Response => {
-      expect(Response.status).to.eq(200)
-      expect(Response.body.name).to.eq('morpheus')
-      expect(Response.body.job).to.eq('zion resident')
-    })
-  })
-})
+      name: 'morpheus',
+      job: 'zion resident',
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.name).to.eq('morpheus');
+      expect(response.body.job).to.eq('zion resident');
+    });
+  });
+});
