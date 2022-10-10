cd6a99015aa36b225381cb023bc557fe42ccf47a
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:28:37 +0300

    Update test_UPDATE.cy.js

--- cypress/e2e/all- tests/API_reqres/test_UPDATE.cy.js
+++ cypress/e2e/all- tests/API_reqres/test_UPDATE.cy.js
@@ -5,9 +5,9 @@ context('Testing PUT and PATCH methods in reqres', () => {
         expect(response.status).to.eq(200);
         expect(response.body.name).to.eq(updateduser.name);
         expect(response.body.job).to.eq(updateduser.job);
-      });
-    });
-  });
+      })
+    })
+  })
 
   it('Positive: update a user by id', () => {
     cy.request('PUT', '/api/users/2', {
@@ -17,6 +17,6 @@ context('Testing PUT and PATCH methods in reqres', () => {
       expect(response.status).to.eq(200);
       expect(response.body.name).to.eq('morpheus');
       expect(response.body.job).to.eq('zion resident');
-    });
-  });
-});
+    })
+  })
+})
