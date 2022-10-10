1153cfc729465e1b2ce27ff25e1dea408d34e83d
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:42:19 +0300

    Add files via upload

---
+++ cypress/support/page_objects/productsPage.js
@@ -0,0 +1,29 @@
+class ProductsPage {
+
+    get product() {
+        return cy.get('.inventory_item');
+    }
+
+    get productName() {
+        return cy.get('.inventory_item_name');
+    }
+
+    get productPrice() {
+        return cy.get('.inventory_item_price');
+    }
+
+    get productsFilter() {
+        return cy.get('.product_sort_container')
+    }
+
+    get shoppingCart() {
+        return cy.get('.shopping_cart_container')
+    }
+
+    get shoppingCartBadge() {
+        return cy.get('.shopping_cart_badge')
+    }
+
+}
+
+export default new ProductsPage()
