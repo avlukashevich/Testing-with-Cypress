0ed1bf43425ddb4da74e8a7b05f356299ea86014
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:35:35 +0300

    Update if_switch.cy.js

--- cypress/e2e/all- tests/Tasks/if_switch.cy.js
+++ cypress/e2e/all- tests/Tasks/if_switch.cy.js
@@ -1,45 +1,45 @@
-import Сhance from 'chance'
+import Сhance from 'chance';
+
 context('Testing if, else and ?', () => {
-    let age = chance.age();
+  const age = chance.age()
 
-    it('TESTS', () => {
-        {
-            //define a person using if operator
-            cy.log('The generated age is' + " " + age);
-            if (age >= 0 && age < 13) {
-                cy.log("It's a child");
-            } else if (age >= 13 && age < 20) {
-                cy.log("It's a teen");
-            } else if (age >= 20 && age < 60) {
-                cy.log("It's an adult");
-            } else {
-                cy.log("It's a senior");
-            }
-        }
+  it('TESTS', () => {
+    {
+      // define a person using if operator
+      cy.log('The generated age is' + ` ${age}`)
+      if (age >= 0 && age < 13) {
+        cy.log("It's a child")
+      } else if (age >= 13 && age < 20) {
+        cy.log("It's a teen")
+      } else if (age >= 20 && age < 60) {
+        cy.log("It's an adult")
+      } else {
+        cy.log("It's a senior")
+      }
+    }
 
-        //define a person using switch
-        switch (true) {
-            case age >= 0 && age < 13:
-                cy.log("It's a child");
-                break;
-            case age >= 13 && age < 20:
-                cy.log("It's a teen");
-                break;
-            case age >= 20 && age < 60:
-                cy.log("It's an adult");
-                break;
-            case age >= 60 && age < 120:
-                cy.log("It's a senior");
-                break;
-        }
+    // define a person using switch
+    switch (true) {
+      case age >= 0 && age < 13:
+        cy.log("It's a child")
+        break;
+      case age >= 13 && age < 20:
+        cy.log("It's a teen")
+        break;
+      case age >= 20 && age < 60:
+        cy.log("It's an adult")
+        break;
+      case age >= 60 && age < 120:
+        cy.log("It's a senior")
+        break;
+    }
 
-        //define a person using ?
-        let isChild = (age >= 0 && age < 13) ? 'isChild' : 'notChild';
-        let isTeen = (age >= 13 && age < 20) ? 'isTeen' : 'notTeen';
-        let isAdult = (age >= 20 && age < 60) ? 'isAdult' : 'notAdult';
-        let isSeniour = (age >= 60 && age < 120) ? 'isSenior' : 'notSenior';
+    // define a person using ?
+    const isChild = (age >= 0 && age < 13) ? 'isChild' : 'notChild'
+    const isTeen = (age >= 13 && age < 20) ? 'isTeen' : 'notTeen'
+    const isAdult = (age >= 20 && age < 60) ? 'isAdult' : 'notAdult'
+    const isSeniour = (age >= 60 && age < 120) ? 'isSenior' : 'notSenior'
 
-        cy.log(isChild + ' ' + isAdult + ' ' + isSeniour + ' ' + isTeen);
-
-    })
+    cy.log(`${isChild} ${isAdult} ${isSeniour} ${isTeen}`)
+  })
 })
