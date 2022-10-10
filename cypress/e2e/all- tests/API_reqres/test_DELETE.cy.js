ef836c9d3bfba041a3a74e6c7d2426a902cb35fc
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:24:35 +0300

    Update test_DELETE.cy.js

--- cypress/e2e/all- tests/API_reqres/test_DELETE.cy.js
+++ cypress/e2e/all- tests/API_reqres/test_DELETE.cy.js
@@ -1,8 +1,7 @@
 context('Testing DELETE method in reqres', () => {
-
-    it('Negative test: delete a single user by id', () => {
-        cy.request({ method: 'DELETE', url: '/api/users/2', failOnStatusCode: false }).then(Response => {
-            expect(Response.status).to.eq(204)
-        })
-    })
-})
+  it('Negative test: delete a single user by id', () => {
+    cy.request({ method: 'DELETE', url: '/api/users/2', failOnStatusCode: false }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  });
+});
