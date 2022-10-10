b4b804c6dde6e3b69fdbe6a0a7582c7bf7118ac7
Sviatlana Tsybulskaya <54994567+svetatt@users.noreply.github.com>
2022-10-10 19:25:49 +0300

    Update test_POST.cy.js

--- cypress/e2e/all- tests/API_reqres/test_POST.cy.js
+++ cypress/e2e/all- tests/API_reqres/test_POST.cy.js
@@ -1,87 +1,89 @@
-import { DATA_OPTIONS, getUserData } from "../../../utils/requestsDataGenerator";
+import { DATA_OPTIONS, getUserData } from '../../../utils/requestsDataGenerator';
 
 context('Testing POST method in reqres', () => {
-
   it('Positive test: create a user', () => {
-    cy.fixture('createUser').then(user => {
-
-      cy.request('POST', '/api/users', user).then(Response => {
-        expect(Response.status).to.eq(201)
-        expect(Response.body).to.have.property('name', user.name)
-        expect(Response.body).to.have.property('job', user.job)
-        expect(Response.body).to.have.property('id', user.id)
-      })
-    })
-  })
+    cy.fixture('createUser').then((user) => {
+      cy.request('POST', '/api/users', user).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body).to.have.property('name', user.name);
+        expect(response.body).to.have.property('job', user.job);
+        expect(response.body).to.have.property('id', user.id);
+      });
+    });
+  });
 
   it('Positive test: register a user', () => {
-    cy.fixture('registerUser').then(user => {
-
-      cy.request('POST', '/api/register', user).then(Response => {
-        expect(Response.status).to.eq(200)
-        expect(Response.body).to.have.property('id', 4)
-        expect(Response.body).to.have.property('token', 'QpwL5tke4Pnpja7X4')
-      })
-    })
-  })
+    cy.fixture('registerUser').then((user) => {
+      cy.request('POST', '/api/register', user).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('id', 4);
+        expect(response.body).to.have.property('token', 'QpwL5tke4Pnpja7X4');
+      });
+    });
+  });
 
   it('Positive test: login an existing user', () => {
-    cy.fixture('loginUser').then(user => {
-
-      cy.request('POST', '/api/login', user).then(Response => {
-        expect(Response.status).to.eq(200)
-        expect(Response.body).to.have.property('token', 'QpwL5tke4Pnpja7X4')
-      })
-    })
-  })
+    cy.fixture('loginUser').then((user) => {
+      cy.request('POST', '/api/login', user).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('token', 'QpwL5tke4Pnpja7X4');
+      });
+    });
+  });
 
   it('Negative test: login unsuccessful', () => {
     cy.request({
-      method: 'POST', url: '/api/login', failOnStatusCode: false, body:
+      method: 'POST',
+      url: '/api/login',
+      failOnStatusCode: false,
+      body:
       {
-        email: "peter@klaven"
-      }
-    }).then(Response => {
-      expect(Response.status).to.eq(400)
-      expect(Response.body).to.have.property('error', 'Missing password')
-    })
-  })
+        email: 'peter@klaven',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('error', 'Missing password');
+    });
+  });
 
   it('Negative test: register unsuccessful', () => {
     cy.request({
-      method: 'POST', url: '/api/register', failOnStatusCode: false, body:
+      method: 'POST',
+      url: '/api/register',
+      failOnStatusCode: false,
+      body:
       {
-        email: "sydney@fife"
-      }
-    }).then(Response => {
-      expect(Response.status).to.eq(400)
-      expect(Response.body).to.have.property('error', 'Missing password')
-    })
-  })
-})
+        email: 'sydney@fife',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('error', 'Missing password');
+    });
+  });
+});
 
 context('Parametrized test for creating a user', () => {
-  let testingData = [
+  const testingData = [
     {
       description: 'Max values',
-      requestData: getUserData(DATA_OPTIONS.MAX)
+      requestData: getUserData(DATA_OPTIONS.MAX),
     },
     {
       description: 'Min values',
-      requestData: getUserData(DATA_OPTIONS.MIN)
+      requestData: getUserData(DATA_OPTIONS.MIN),
     },
     {
       description: 'Random Average values',
-      requestData: getUserData(DATA_OPTIONS.AVERAGE)
-    }
-  ]
+      requestData: getUserData(DATA_OPTIONS.AVERAGE),
+    },
+  ];
   testingData.forEach(({ description, requestData }) => {
     it(`Positive: POST request - create a user (random values) - ${description}`, () => {
       cy.request('POST', '/api/users', requestData).then((response) => {
-        expect(response.status).to.eq(201)
-        expect(response.body).to.have.property('name', requestData.name)
-        expect(response.body).to.have.property('job', requestData.job)
-      })
-    })
-  })
-})
+        expect(response.status).to.eq(201);
+        expect(response.body).to.have.property('name', requestData.name);
+        expect(response.body).to.have.property('job', requestData.job);
+      });
+    });
+  });
+});
